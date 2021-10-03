f1c43e5c804ad5bc516b3f532edf9d56e8d9841b
전병훈 <83635402+Carolsuki@users.noreply.github.com>
2021-10-04 01:22:04 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -4,7 +4,7 @@ int main(void){
 	int num1 = 0;
 	int num2 = 0;
 	
-	printf("µÎ Á¤¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä: ");
+	printf("ë‘ ì •ìˆ˜ë¥¼ ì…ë ¥í•˜ì„¸ìš”: ");
 	scanf("%d %d", &num1, &num2);
 	
 	printf("%d & %d = %d\n", num1, num2, num1 & num2);
